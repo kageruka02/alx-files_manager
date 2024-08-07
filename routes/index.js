70f85a39818c5e3c3d434ee2a10ed64408658153
kageruka02 <leonkageruka@gmail.com>
2024-08-07 19:01:11 +0300

    connnect to express server

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 
 const router = Router();
-router.get('/status', AppController.getStatus());
-router.get('/stats', AppController.getStats());
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 export default router;
