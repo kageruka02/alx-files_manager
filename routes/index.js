343ed8c08c33328b6fcc50884d4c2778c873afc3
kageruka02 <leonkageruka@gmail.com>
2024-08-08 10:34:12 +0300

    correction

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 
 import AppController from '../controllers/AppController';
-import FilesController from '../controllers/FilesController';
 
 const router = Router();
 router.get('/status', AppController.getStatus);
@@ -12,5 +11,5 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload);
+
 export default router;
