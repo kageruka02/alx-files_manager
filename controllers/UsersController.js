8875f8bcaba86856428980a33f88f1468f31ba4d
kageruka02 <leonkageruka@gmail.com>
2024-08-08 22:58:03 +0300

    handling invalid header

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,6 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+// import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -22,23 +22,28 @@ class UsersController {
     return res.status(201).json({ id: newUser.insertedId, email });
   }
 
+  // static async getMe(req, res) {
+  //   const token = req.header('X-Token');
+  //   if (!token) {
+  //     return res.status(401).json({ error: 'Unauthorized' });
+  //   }
+  //   const tokenKey = `auth_${token}`;
+  //   const userId = await redisClient.get(tokenKey);
+  //   if (!userId) {
+  //     return res.status(401).json({ error: 'Unauthorized' });
+  //   }
+  //   const user = await dbClient.db
+  //     .collection('users')
+  //     .findOne({ _id: dbClient.getObjectId(userId) });
+  //   if (!user) {
+  //     return res.status(401).json({ error: 'Unauthorized' });
+  //   }
+  //   return res.status(200).json({ id: user._id, email: user.email });
+  // }
   static async getMe(req, res) {
-    const token = req.header('X-Token');
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    const tokenKey = `auth_${token}`;
-    const userId = await redisClient.get(tokenKey);
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    const user = await dbClient.db
-      .collection('users')
-      .findOne({ _id: dbClient.getObjectId(userId) });
-    if (!user) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    return res.status(200).json({ id: user._id, email: user.email });
+    const { user } = req;
+
+    res.status(200).json({ email: user.email, id: user._id.toString() });
   }
 }
 export default UsersController;
