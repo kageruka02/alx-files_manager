8240f39927696346e34031c03457d6c97cb8174f
kageruka02 <leonkageruka@gmail.com>
2024-08-09 00:02:58 +0300

    handling invalid header

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -34,7 +34,7 @@ class UsersController {
     }
     const user = await dbClient.db
       .collection('users')
-      .findOne({ _id: ObjectId(userId) });
+      .findOne({ _id: dbClient.ObjectId(userId) });
     if (!user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
