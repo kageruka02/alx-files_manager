893e5ab835b8f3bb7dfb5121c5b3221ac906d5e1
kageruka02 <leonkageruka@gmail.com>
2024-08-10 00:29:43 +0300

    file creation

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 
 import AppController from '../controllers/AppController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 router.get('/status', AppController.getStatus);
@@ -11,5 +12,6 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
 
 export default router;
